1e9b5bff7c57b1f8a3f4aed03e344269088c8b01
MrPakoras <hartanas8g@gmail.com>
2020-12-26 09:04:10 +0000

    Update MC RandPack v0.4.py

--- MC RandPack v0.4.py
+++ MC RandPack v0.4.py
@@ -7,64 +7,81 @@ from colorama import Fore
 colorama.init(autoreset=True)
 
 rootdir = './1.16.4/assets/minecraft/textures/'
-texdirs = ['block/','entity/','item/','map/','mob_effect/','models/armor/','painting/','particle/'] # Texture directories
+texdirs = ['block/','item/','map/','mob_effect/','models/armor/','painting/','particle/'] # Texture directories
 
 dt = time.strftime('-%d%m%y-%H%M%S')
 
 # Excluded files
 exclfiles = ['water_flow.png', 'water_overlay.png', 'water_still.png']
 # Coloursed files
-colfiles = ['lava_flow.png','lava_still.png','redstone_block.png']
+colfiles = ['lava_flow.png','lava_still.png','redstone_block.png','end_portal.png','bedrock.png','stone.png']
 
 
 def cfg(imagepath, filename): # Colourise from greyscale 
-	img = i.open(imagepath)
+	try:
+		img = i.open(imagepath)
 
-	cb, cw = random.randint(0,125), random.randint(126,255)
-	img = img.convert('L') # Convert to greyscale
-	img = ImageOps.colorize(img, black=cb, white=cw) # colour map
+		cb, cw = random.randint(0,125), random.randint(126,255)
+		img = img.convert('L') # Convert to greyscale
+		img = ImageOps.colorize(img, black=(cb,cb,cb), white=(cw,cw,cw)) # colour map
 
-	img.save(f'{newdir}{filename}', format='PNG')
+		img.save(f'{newdir}{filename}', format='PNG')
+
+	except FileNotFoundError:
+		pass
 
 def rgbswap(imagepath, filename): # Swaps RGB value with each other
-	img = i.open(imagepath)
-	pixels = img.load()
+	try:
+		img = i.open(imagepath)
+		pixels = img.load()
 
-	# create array of all colours in image and colours they should be mapped to
-	pcdict = {} # Pixel colour dictionary
-	
-	for x in range(img.size[0]):
-		for y in range(img.size[1]):
-			try:
-				p = pixels[x,y]
+		# create array of all colours in image and colours they should be mapped to
+		pcdict = {} # Pixel colour dictionary
+		
+		for x in range(img.size[0]):
+			for y in range(img.size[1]):
+				try:
+					p = pixels[x,y]
 
-				if len(p) == 4 and p[3] == 0:
+					if len(p) == 4 and p[3] == 0:
+						pass
+					else:
+
+						if len(p) == 3:
+							r,g,b = p
+							alpha = 255
+							rgba = (r,g,b,alpha)
+						elif len(p) == 4:
+							rgba = p
+						else:
+							continue
+
+						if rgba not in pcdict:
+							newrgba = random.sample(rgba[:3],3)
+							newrgba.append(rgba[3]) # Add alpha value
+							newrgba = tuple(newrgba)
+							pcdict[rgba] = newrgba
+							pixels[x,y] = newrgba
+
+						else:
+							pixels[x,y] = pcdict[rgba]
+
+				except TypeError:
 					pass
-				else:
+		# img = img.convert('RGBA')
+		img.save(f'{newdir}{filename}', format='PNG')
 
-					if len(p) == 3:
-						r,g,b = p
-						alpha = 255
-						rgba = (r,g,b,alpha)
-					elif len(p) == 4:
-						rgba = p
-					else:
-						continue
+	except FileNotFoundError:
+		pass
 
-					if rgba not in pcdict:
-						newrgba = random.sample(rgba[:3],3)
-						newrgba.append(255) # Add alpha value of 255
-						newrgba = tuple(newrgba)
-						pcdict[rgba] = newrgba
-						pixels[x,y] = newrgba
 
-					else:
-						pixels[x,y] = pcdict[rgba]
 
-			except TypeError:
-				pass
-	# img = img.convert('RGBA')
-	img.save(f'{newdir}{filename}', format='PNG')
+for dirName, subdirList, fileList in os.walk(f'{rootdir}entity/'):
+	for subdirname in subdirList:
+		texdirs.append(f'entity/{subdirname}/')
+		newdir = f'./MCRandPack{dt}/assets/minecraft/textures/entity/{subdirname}' 
+		Path(newdir).mkdir(parents=True, exist_ok=True) # Make new directory for edited textures
+
 
 for folder in texdirs:
 	newdir = f'./MCRandPack{dt}/assets/minecraft/textures/{folder}' 
@@ -81,5 +98,18 @@ for folder in texdirs:
 				else:
 					rgbswap(filename, fname[1])
 					print(f'{Fore.CYAN}>> {fname[0]} - {Fore.GREEN}{fname[1]}')
+
+		
+		# for fname in enumerate(fileList, start=1):
+		# 	filename = f'{rootdir}entity/{subdirname}/{fname[1]}'
+		# 	if filename[-4:] == '.png':
+		# 		if fname[1] in exclfiles:
+		# 			print(f'{Fore.CYAN}>> {fname[0]} - {Fore.RED}{fname[1]}')
+		# 		elif fname[1] in colfiles:
+		# 			cfg(filename, fname[1])
+		# 		else:
+		# 			rgbswap(filename, fname[1])
+		# 			print(f'{Fore.CYAN}>> {fname[0]} - {Fore.GREEN}{fname[1]}')
+
 
 copy2('./pack.mcmeta',f'./MCRandPack{dt}/') # Copies .mcmeta file to new directory
